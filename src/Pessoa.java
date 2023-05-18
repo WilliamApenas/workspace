6b6b48c0b2b78b7a3b16e1446a36844882ec832a
WilliamApenas <williamabreu687@gmail.com>
2023-05-18 11:05:49 -0300

    ababa

---
+++ src/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+
+    //atributos
+    public String nome;
+    public String sobrenome;
+
+    //métodos da classe
+    public void falar(){
+        System.out.println("falei");
+    }
+}
